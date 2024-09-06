7db4216a9a6489d656822a1f7cc5b791acc23e16
lewfer <lif@arcwave.co.uk>
2024-09-06 18:20:04 +0100

    Fixed HTML

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Mypage web app
+
 var http = require("http"); 
 const port = 8000; 
 
 http.createServer(function(req, res) { 
-    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.writeHead(200, { "Content-Type": "text/html" });
     res.end("<!doctype html><html><head><title>MyPage</title></head><h1>This is me</h1></body></html>");
     }).listen(port, function() { 
         console.log(`Node server is running on port ${port}...`); 
